ca46eb7733219516f1e072cbc1bfccadf630f2d8
Serge Semenov <sergii.semenov@skyKick.com>
2017-02-05 11:26:52 -0800

    Bump version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,14 +2,14 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 [assembly: AssemblyTitle("AsyncWaitHandle")]
 [assembly: AssemblyProduct("AsyncWaitHandle")]
 [assembly: AssemblyDescription("async-await extensions for WaitHandle")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Serge Semenov")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
